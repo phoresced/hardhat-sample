a95c12c5b0197880e64f16f3dc6f0b8a7d155591
phoresced <62972420+phoresced@users.noreply.github.com>
2022-08-25 18:33:27 +1200

    Define events

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -3,6 +3,21 @@ pragma solidity ^0.8.4;
 
 contract Web3RSVP {
 
+  event NewEventCreated(
+    bytes32 eventID,
+    address creatorAddress,
+    uint256 eventTimestamp,
+    uint256 maxCapacity,
+    uint256 deposit,
+    string eventDataCID
+  );
+
+  event NewRSVP(bytes32 eventID, address attendeeAddress);
+
+  event ConfirmedAttendee(bytes32 eventID, address attendeeAddress);
+
+  event DepositsPaidOut(bytes32 eventID);
+
   struct CreateEvent {
       bytes32 eventId;
       string eventDataCID;
